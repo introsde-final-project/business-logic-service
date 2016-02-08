33ff4a929515a42162dd86aaf863d901e2789f7c
Bishruti Siku <bishrutisiku7@gmail.com>
2016-02-08 01:42:57 +0100

    Add imports.

--- src/business/server/resources/UserResource.java
+++ src/business/server/resources/UserResource.java
@@ -5,7 +5,10 @@ import business.server.model.User;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
 import java.util.List;
 
